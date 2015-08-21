f954a7ec4c8e9868bc4e2af4b31a72e16f1a62ff
Kyle Richardson <s.k.richardson7@gmail.com>
2015-08-20 20:27:43 -0400

    Added success action in PHP form -mySQL not connected

--- invite.php
+++ invite.php
@@ -6,8 +6,14 @@
     }
 
     //Check if email is entered and valid
-    if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-      $errEmail = 'Please enter a valid email address';
+    if (!$_POST["email"] || !filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+      $errorEmail = "Please Enter a Valid Email Address";
     }
+
+  // If Email is present and Valid Send Request
+  if (!$errorEmail) {
+  	if (mail ($to, $subject, $body, $from)) {
+  		$result='<div class="alert alert-success">Thank you interest in Mattr. We will be in touch soon!</div>';
+  	}
 
 ?>
