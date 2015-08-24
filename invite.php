6a0d13334ef61ce953a4aeb245f8c1bf8b084002
Kyle Richardson <s.k.richardson7@gmail.com>
2015-08-24 12:23:00 -0400

    beginning Stages of implmenting php form and connecting to mySQL

--- invite.php
+++ invite.php
@@ -5,6 +5,7 @@
       $email = $_POST["email"];
     }
 
+  else{
     //Check if email is entered and valid
     if (!$_POST["email"] || !filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
       $errorEmail = "Please Enter a Valid Email Address";
@@ -15,5 +16,26 @@
   	if (mail ($to, $subject, $body, $from)) {
   		$result='<div class="alert alert-success">Thank you interest in Mattr. We will be in touch soon!</div>';
   	}
-
+  else{
+      $dbhost = 'localhost:8888';
+      $dbuser = 'root';
+      $dbpass = 'rootpassword';
+      $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+      if(! $conn )
+      {
+        die('Could not connect: ' . mysql_error());
+      }
+      // $sql = 'INSERT INTO request '.
+      //        '(email) '.
+      //        'VALUES ( "email");
+      //
+      // mysql_select_db('test_db');
+      // $retval = mysql_query( $sql, $conn );
+      // if(! $retval )
+      // {
+      //   die('Could not enter data: ' . mysql_error());
+      // }
+      echo "Entered data successfully\n";
+      mysql_close($conn);
+    }
 ?>
